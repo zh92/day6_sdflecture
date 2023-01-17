6d0775a9fe05bf2a42f7c264c809c8543f294f68
zh92 <zhehao92@gmail.com>
2023-01-17 13:38:20 +0800

    Lambda expression with comparator function

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,7 +1,11 @@
 package sg.edu.nus.iss;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -17,28 +21,28 @@ public final class App {
      */
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread(new Runnable() {
+        // Thread thread1 = new Thread(new Runnable() {
 
-            @Override
-            public void run() {
-                for (int i = 0; i < 20; i++) {
-                    System.out.println(Thread.currentThread().getName() +
-                            "\tRunnable ..." + i);
-                }
-            }
-        });
-        thread1.start();
+        //     @Override
+        //     public void run() {
+        //         for (int i = 0; i < 20; i++) {
+        //             System.out.println(Thread.currentThread().getName() +
+        //                     "\tRunnable ..." + i);
+        //         }
+        //     }
+        // });
+        // thread1.start();
 
-        MyRunnableImplementation mRI1 = new MyRunnableImplementation("Task1");
+        // MyRunnableImplementation mRI1 = new MyRunnableImplementation("Task1");
         // MyRunnableImplementation mRI2 = new MyRunnableImplementation("Task2");
         // MyRunnableImplementation mRI3 = new MyRunnableImplementation("Task3");
         // MyRunnableImplementation mRI4 = new MyRunnableImplementation("Task4");
         // MyRunnableImplementation mRI5 = new MyRunnableImplementation("Task5");
-        Thread thread2 = new Thread(mRI1);
-        thread2.start();
+        // Thread thread2 = new Thread(mRI1);
+        // thread2.start();
 
-        Thread thread3 = new Thread(mRI1);
-        thread3.start();
+        // Thread thread3 = new Thread(mRI1);
+        // thread3.start();
 
         // ExecutorService executorService = Executors.newSingleThreadExecutor();
         // executorService.execute(mRI1);
@@ -84,5 +88,34 @@ public final class App {
         // System.out.println("concatenateOperation: " + concatenateString.process("The brown fox ",
         //  "jump over the wall"));
         // printString.printMessage("Let's take a break a 12pm");
+
+        //List of employees
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add(new Employee(1, "Tom", "Tan", 3000));
+        employees.add(new Employee(2, "Alex", "Lim", 5000));
+        employees.add(new Employee(3, "David", "Chan", 6000));
+        employees.add(new Employee(4, "Richard", "Ho", 2000));
+
+        // employees.forEach(emp -> System.out.println(emp));
+
+        List<Employee> for (Employee filteredEmployees : filteredEmployees = employees.stream().filter(emp -> emp.getLastName().contains("Lim"))
+                .collect(Collectors.toList());
+                // filteredEmployees.forEach(emp -> System.out.println(emp));
+        
+                // employees.sort(Comparator.comparing(e -> e.getFirstName()));
+                //Using comparator 
+                // employees.sort(Comparator.comparing(Employee::getFirstName));
+                employees.sort(Comparator.comparing(Employee::getFirstName).reversed());
+        
+                Comparator<Employee> compare = Comparator.comparing(e -> e.getFirstName());
+                employees.sort(compare.reversed());
+                // employees.forEach(emp -> System.out.println(emp));
+        
+                Comparator<Employee> groupByComparator = Comparator.comparing(Employee::getFirstName)
+                .thenComparing(Employee::getLastName);
+                employees.sort(groupByComparator);
+                employees.forEach(emp -> System.out.println(exp));
+            
+        }
     }
 }
---
+++ src/main/java/sg/edu/nus/iss/Employee.java
@@ -0,0 +1,32 @@
+package sg.edu.nus.iss;
+
+public class Employee {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer salary;
+
+    public Employee(Integer id, String firstName, String lastName, Integer salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) {this.id = id;}
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public Integer getSalary() {return salary; }
+    public void setSalary(Integer salary) {this.salary = salary;}
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+                + "]";
+    }
+    
+    
+}
